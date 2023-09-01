40aad5bfd658ad1301fae94f9bc8ef8828cbe465
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-01 10:51:45 +0200

    Migrate database

--- db/migrate/20230901084018_add_reference_to_user_on_events.rb
+++ db/migrate/20230901084018_add_reference_to_user_on_events.rb
@@ -1,5 +1,5 @@
 class AddReferenceToUserOnEvents < ActiveRecord::Migration[7.0]
   def change
-    add_reference :user_on_events, :user, null: false, foreign_key: true
+    add_reference :events, :user, null: false, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_01_074916) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_084018) do
   create_table "events", force: :cascade do |t|
     t.string "location"
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_01_074916) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
