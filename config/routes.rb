09e56dc2ce5e8b980fe13c2f051c1fe2a4cd2d02
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-04 11:17:48 +0200

    Add New Event link on index page. Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show]
-  get 'events/index'
+  
+  # get 'events/index'
+  resources :events, only: [:index, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
