91544088bf9d0ff7305e9afc5b6fee2e1c0d4614
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-05 10:56:27 +0200

    Update User and Event models with has_many associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   # belongs_to :user
+  
+  has_many :signups, foreign_key: :attended_event_id
+  has_many :attendees, through: :signups, source: :event_attendee
+
   belongs_to :creator, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :signups, foreign_key: :event_attendee_id
+  has_many :attended_events, through: :signups
+
   has_many :events, foreign_key: :creator_id
 end
