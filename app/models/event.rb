d180c998c44d4addfd853c5e1dcf64936bb74325
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-06 14:08:58 +0200

    Filter for Past and Future events on events#index view using Event model class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,12 @@ class Event < ApplicationRecord
   has_many :attendees, through: :event_users, source: :event_attendee
 
   belongs_to :creator, class_name: 'User'
+
+  def self.past
+    where("date < :today_date", {today_date: Date.today}).order(date: :asc)
+  end
+
+  def self.future
+    where("date > :today_date", {today_date: Date.today}).order(date: :asc)
+  end
 end
