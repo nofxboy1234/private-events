1a88a38b4a735752c7218849e88e006b476b8637
Dylan Palmboom <nofxboy1234@gmail.com>
2023-09-06 14:25:46 +0200

    Refactor the past and future class methods into simple scopes. Call these from User show page and Event index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,12 @@
 class Event < ApplicationRecord
   # belongs_to :user
-  
   has_many :event_users, foreign_key: :attended_event_id
   has_many :attendees, through: :event_users, source: :event_attendee
-
+  
   belongs_to :creator, class_name: 'User'
+
+  scope :past_events, -> { where("date < :today_date", {today_date: Date.today}).order(date: :asc) }
+  scope :future_events, -> { where("date > :today_date", {today_date: Date.today}).order(date: :asc) }
 
   def self.past
     where("date < :today_date", {today_date: Date.today}).order(date: :asc)
